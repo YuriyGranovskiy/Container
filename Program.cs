7940df9fea6ed1ab745900bb17bced453a099b6f
Yuriy Granovskiy <Yuriy.Granovskiy@gmail.com>
2018-10-20 00:24:26 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Container
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                throw new ArgumentException("Not enough arguments");
+            }
+
+            switch (args[0])
+            {
+                case "run":
+                    {
+                        Run(args.Skip(1));
+                        break;
+                    }
+                default:
+                    throw new InvalidOperationException("WAT?");
+            }
+
+        }
+
+        private static void Run(IEnumerable<string> args)
+        {
+            ProcessStartInfo processInfo = new ProcessStartInfo();
+            processInfo.FileName = args.First();
+            processInfo.Arguments = string.Join(" ", args.Skip(1));
+            var process = new Process{ StartInfo = processInfo};
+            process.Start();
+            process.WaitForExit();
+        }
+    }
+}
